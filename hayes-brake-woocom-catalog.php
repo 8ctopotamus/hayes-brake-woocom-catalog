c57789cf3dde46f934ba28cf949a7f1164c604af
8ctopotamus <joshnaylor88@gmail.com>
2017-06-27 12:36:53 -0500

    added ACF warning, added ACF custom fields to codebase

--- hayes-brake-woocom-catalog.php
+++ hayes-brake-woocom-catalog.php
@@ -12,13 +12,27 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+// ACF needs to be installed and activated
+function my_acf_notice() { ?>
+	<div class="update-nag notice">
+	  <p><?php _e( 'Please install the Advanced Custom Fields and ACF Repeater Fields plugins (Both included in ACF PRO), they are required for the "Hayes Brake WooCommerce Catalog" plugin to work properly!', 'my_plugin_textdomain' ); ?></p>
+	</div>
+<?php }
+
+//check for ACF. If available, run plugin
+if( !class_exists('acf') || !function_exists( 'the_field') ) {
+	add_action( 'admin_notices', 'my_acf_notice' );
+	return;
+}
+
 function hayes_cat_register_scripts_and_styles() {
 	wp_enqueue_style( 'hayes-cat-style', plugins_url( '/css/custom.css',  __FILE__ ));
-	//wp_enqueue_script( 'simply-countdown-script', plugins_url( '/js/simplyCountdown.js',  __FILE__ ), array(), '', true);
+	//wp_enqueue_script( 'example-script', plugins_url( '/path/to/file.js',  __FILE__ ), array(), '', true);
 }
 add_action('wp_enqueue_scripts', 'hayes_cat_register_scripts_and_styles');
 
 // disables/moves various parts of the WooCommerce Templates
+include( plugin_dir_path( __FILE__ ) . 'includes/acf-custom-fields.php');
 include( plugin_dir_path( __FILE__ ) . 'includes/woocommerce-customization-hooks.php');
 include( plugin_dir_path( __FILE__ ) . 'includes/experts-spec-sheet-buttons.php');
 include( plugin_dir_path( __FILE__ ) . 'includes/woocommerce-custom-title.php');
